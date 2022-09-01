43ecde0c823687c24a88ecd30ca23432f1f1d9a8
Alexander <Sakeran@gmail.com>
2022-09-01 18:47:30 -0400

    Fix preprocessor setup

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,21 +1,21 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"
+    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/svelte",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: "@storybook/svelte",
+  core: {
+    builder: "@storybook/builder-vite",
   },
-  "svelteOptions": {
-    "preprocess": import("../svelte.config.js").preprocess
+  svelteOptions: {
+    preprocess: require("svelte-preprocess")({}),
   },
-  "features": {
-    "storyStoreV7": true
-  }
-}
+  features: {
+    storyStoreV7: true,
+  },
+};
