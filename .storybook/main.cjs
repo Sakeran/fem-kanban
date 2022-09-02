ddf53a51b6ba68a03edb8505ec3c68c97e61173e
Alexander <Sakeran@gmail.com>
2022-09-01 20:05:28 -0400

    Add svelte-csf addon

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -7,6 +7,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-svelte-csf",
   ],
   framework: "@storybook/svelte",
   core: {
