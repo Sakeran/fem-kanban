c587b6fcd75f66c0f5314bc6c620227e4ee5fa99
Alexander <Sakeran@gmail.com>
2022-09-07 16:19:36 -0400

    Implement Text component

---
+++ src/components/Typography/Text/Text.stories.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+  import Text from "./Text.svelte";
+</script>
+
+<Meta
+  title="Typography/Text"
+  component={Text}
+  argTypes={{
+    element: { control: "select", options: ["p", "label", "span"] },
+    style: { control: "select", options: ["L", "M"] },
+    classes: { control: "text" },
+  }}
+/>
+
+<Template let:args>
+  <div style="max-width: 40ch;">
+    <Text {...args}>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa, error
+      facere? Minus iste odit repudiandae minima reiciendis, earum a inventore
+      nihil autem sed quidem molestias dolorem ad aliquid, delectus nisi.
+    </Text>
+  </div>
+</Template>
+
+<Story name="Text L (paragraph, default)" args={{}} />
+
+<Story name="Text L (paragraph, explicit)" args={{ element: "p", style: "L" }} />
+
+<Story name="Text M" args={{ element: "span", style: "M" }} />
+
+<Story
+  name="Text M (Extra Classes)"
+  args={{ element: "span", style: "M", classes: "text-gray-medium" }}
+/>
---
+++ src/components/Typography/Text/Text.svelte
@@ -0,0 +1,17 @@
+<script context="module" lang="ts">
+  const styles = {
+    L: "font-medium leading-23/13 text-13p",
+    M: "font-bold leading-15/12 text-12p",
+  };
+</script>
+
+<script lang="ts">
+  export let element: "span" | "p" | "label" = "p";
+  export let style: "M" | "L" = "L";
+  export let classes: string = "";
+
+  let classList = styles[style];
+  if (classes) classList += " " + classes;
+</script>
+
+<svelte:element this={element} class={classList}><slot /></svelte:element>
