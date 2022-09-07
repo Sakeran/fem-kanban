31f04e0c172e89ee192c146f9d7d34d82feac039
Alexander <Sakeran@gmail.com>
2022-09-07 12:29:45 -0400

    Add typography config to TailwindCSS

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -24,12 +24,27 @@ module.exports = {
     },
 
     fontFamily: {
-      "sans": ["Plus Jakarta Sans", "sans-serif"]
+      sans: ["Plus Jakarta Sans", "sans-serif"],
     },
 
-    fontSize: {},
+    fontSize: {
+      "12p": "0.75rem", // HeadingS, BodyM
+      "13p": "0.8125rem", // BodyL
+      "15p": "0.9375rem", // HeadingM
+      "18p": "1.125rem", // HeadingL
+      "24p": "1.5rem", // HeadingXL
+    },
 
-    lineHeight: {},
+    lineHeight: {
+      "15/12": "1.25", // HeadingXL, HeadingS, BodyM
+      "19/15": "1.266", // HeadingM
+      "23/18": "1.278", // HeadingL
+      "23/13": "1.769", // BodyL
+    },
+
+    tracking: {
+      "24p": "0.15rem",
+    },
 
     extend: {
       spacing: {},
