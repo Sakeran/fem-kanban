fcb885a7142cf8033eb28227cdfc5b7271224cfa
Alexander <Sakeran@gmail.com>
2022-09-08 12:35:47 -0400

    Implement Button component

---
+++ src/components/Typography/Interactive/Button/Button.stories.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+  import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+  import Button from "./Button.svelte";
+</script>
+
+<Meta
+  title="Interactive/Button"
+  component={Button}
+  argTypes={{
+    content: { control: "text", defaultValue: "Button" },
+    element: { control: "select", options: ["button", "a"] },
+    style: {
+      control: "select",
+      options: ["PrimaryL", "PrimaryS", "Secondary", "Destructive"],
+    },
+    classes: { control: "text" },
+  }}
+  parameters={{ actions: { handles: ["click"] } }}
+/>
+
+<Template let:args>
+  <Button {...args}>{args.content}</Button>
+</Template>
+
+<Story name="PrimaryL (Default)" args={{ content: "Button Primary (L)" }} />
+
+<Story
+  name="PrimaryL (Explicit)"
+  args={{ content: "Button Primary (L)", style: "PrimaryL" }}
+/>
+
+<Story
+  name="PrimaryL (Link)"
+  args={{ content: "Button Primary (L)", element: "a" }}
+/>
+
+<Story
+  name="PrimaryS"
+  args={{ content: "Button Primary (S)", style: "PrimaryS" }}
+/>
+
+<Story
+  name="Secondary"
+  args={{ content: "Button Secondary", style: "Secondary" }}
+/>
+
+<Story
+  name="Destructive"
+  args={{ content: "Button Destructive", style: "Destructive" }}
+/>
+
+<Story
+  name="Primary L (Extra Classes)"
+  args={{
+    content: "Button Primary Block",
+    style: "PrimaryL",
+    classes: "w-full",
+  }}
+/>
---
+++ src/components/Typography/Interactive/Button/Button.svelte
@@ -0,0 +1,30 @@
+<script context="module" lang="ts">
+  const styles = {
+    PrimaryL:
+      "font-bold text-15p leading-19/15 bg-main-purple-normal hocus:bg-main-purple-hover text-white outline-2 focus-visible:outline outline-offset-4 focus-visible:outline-main-purple-hover min-h-button-l inline-grid text-center place-items-center rounded-full px-15.5 motion-safe:transition-colors motion-safe:ease-in-out",
+    PrimaryS:
+      "font-bold text-13p leading-23/13 bg-main-purple-normal hocus:bg-main-purple-hover text-white outline-2 focus-visible:outline outline-offset-4 focus-visible:outline-main-purple-hover min-h-button-s inline-grid text-center place-items-center rounded-full px-15.5 motion-safe:transition-colors motion-safe:ease-in-out",
+    Secondary:
+      "font-bold text-13p leading-23/13 bg-main-purple-normal/10 hocus:bg-main-purple-hover/25 dark:bg-white text-main-purple-normal outline-2 focus-visible:outline outline-offset-4 focus-visible:outline-main-purple-hover dark:focus-visible:outline-white min-h-button-s inline-grid text-center place-items-center rounded-full px-15.5 motion-safe:transition-colors motion-safe:ease-in-out",
+    Destructive:
+      "font-bold text-13p leading-23/13 bg-red-normal hocus:bg-red-hover text-white outline-2 focus-visible:outline outline-offset-4 focus-visible:outline-red-hover min-h-button-s inline-grid text-center place-items-center rounded-full px-15.5 motion-safe:transition-colors motion-safe:ease-in-out",
+  };
+</script>
+
+<script lang="ts">
+  export let style: "PrimaryL" | "PrimaryS" | "Secondary" | "Destructive" =
+    "PrimaryL";
+  export let element: "a" | "button" = "button";
+  export let href: string = "";
+  export let classes: string = "";
+
+  let classList = styles[style];
+  if (classes) classList += " " + classes;
+</script>
+
+<svelte:element
+  this={element}
+  on:click
+  href={element !== "a" ? null : href || "#"}
+  class={classList}><slot /></svelte:element
+>
