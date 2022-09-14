a71d9d7dacf5c46bb401e824f6f2d6d54afa3258
Alexander <Sakeran@gmail.com>
2022-09-14 15:26:06 -0400

    Implement InputList component

---
+++ src/components/Interactive/InputList/InputList.stories.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+  import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+  import { userEvent, within } from "@storybook/testing-library";
+  import { expect } from "@storybook/jest";
+  import InputList from "./InputList.svelte";
+</script>
+
+<Meta
+  title="Interactive/InputList"
+  component={InputList}
+  argTypes={{
+    label: { control: "text" },
+    buttonText: { control: "text" },
+    items: { control: "array" },
+  }}
+/>
+
+<Template let:args>
+  <div style="max-width: 26rem;"><InputList {...args} /></div>
+</Template>
+
+<Story name="Empty" args={{ label: "Subtasks", items: [] }} />
+
+<Story
+  name="Existing Items"
+  args={{
+    label: "Subtasks",
+    items: ["Define User Model", "Add auth endpoints"],
+  }}
+/>
+
+<Story
+  name="Delete Items"
+  args={{
+    label: "Subtasks",
+    items: ["Define User Model", "Add auth endpoints"],
+  }}
+  play={async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const deleteButton = canvas.getByText("Delete subtask Define User Model");
+
+    await userEvent.click(deleteButton);
+
+    const deletedInput = canvas.queryByLabelText("Define User Model");
+    expect(deletedInput).not.toBeInTheDocument();
+  }}
+/>
+
+<Story
+  name="Add New Items"
+  args={{
+    label: "Subtasks",
+    items: ["Define User Model", "Add auth endpoints"],
+  }}
+  play={async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const addButton = canvas.getByText("Add New Subtask");
+
+    await userEvent.click(addButton);
+
+    const newInput = canvas.getByLabelText("blank subtask");
+    await userEvent.type(newInput, "Do another thing");
+  }}
+/>
---
+++ src/components/Interactive/InputList/InputList.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+  import { tick } from "svelte";
+  import Button from "../Button/Button.svelte";
+  import TextInput from "../TextInput/TextInput.svelte";
+
+  export let label: string;
+  export let buttonText: string = "Add New Subtask";
+  export let items: string[] = [];
+
+  if (items.length === 0) items.push("");
+
+  let inputContainer: HTMLDivElement;
+  function handleAddItem() {
+    items.push("");
+    items = items;
+
+    tick().then(() => {
+      const newInput = inputContainer.querySelector("input:last-of-type");
+      if (newInput) (newInput as HTMLInputElement).focus();
+    });
+  }
+
+  function handleDeletion(itemIdx: number) {
+    // Don't delete non-existent entries
+    if (itemIdx >= items.length) return;
+    // Always show at least one subtask box
+    if (items.length === 1) return;
+
+    items.splice(itemIdx, 1);
+    items = items;
+  }
+</script>
+
+<div>
+  <h3 class="text-12p font-bold text-gray-medium dark:text-white">{label}</h3>
+  <div class="mt-2 flex flex-col gap-3">
+    {#each items as item, idx}
+      <div class="flex gap-1" bind:this={inputContainer}>
+        <TextInput
+          label={item || "blank subtask"}
+          placeholder="e.g. Make coffee"
+          classes="basis-full"
+          required
+          visuallyHideLabel={true}
+          bind:value={item}
+        />
+        <button
+          class="text-gray-medium hocus:text-red-normal focus-visible:outline focus-visible:outline-red-normal aspect-square p-3"
+          on:click={() => handleDeletion(idx)}
+        >
+          <svg
+            width="15"
+            height="15"
+            viewBox="0 0 15 15"
+            aria-hidden="true"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              x="12.7275"
+              width="3"
+              height="18"
+              transform="rotate(45 12.7275 0)"
+              fill="currentColor"
+            />
+            <rect
+              y="2.12132"
+              width="3"
+              height="18"
+              transform="rotate(-45 0 2.12132)"
+              fill="currentColor"
+            />
+          </svg>
+
+          <span class="sr-only"
+            >Delete {item ? "subtask " + item : "blank subtask"}</span
+          >
+        </button>
+      </div>
+    {/each}
+  </div>
+  <Button style="Secondary" classes="block w-full mt-3" on:click={handleAddItem}
+    ><span><span aria-hidden="true">+ </span>{buttonText}</span></Button
+  >
+</div>
+
+<style lang="postcss">
+</style>
