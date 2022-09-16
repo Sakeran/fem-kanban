b56bde74d99fa6c349966c2fc06307356ed911fe
Alexander <Sakeran@gmail.com>
2022-09-16 12:38:48 -0400

    Implement IconButton component

---
+++ src/components/Interactive/IconButton/IconButton.stories.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+  import { userEvent, within } from "@storybook/testing-library";
+  import IconButton from "./IconButton.svelte";
+</script>
+
+<Meta
+  title="Interactive/IconButton"
+  component={IconButton}
+  argTypes={{
+    textAlternative: { control: "text" },
+  }}
+  parameters={{ actions: { handles: ["click"] } }}
+/>
+
+<Template let:args>
+  <IconButton {...args}
+    ><svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M7.368 12V7.344H12V4.632H7.368V0H4.656V4.632H0V7.344H4.656V12H7.368Z"
+        fill="white"
+      />
+    </svg>
+  </IconButton>
+</Template>
+
+<Story name="Default" args={{ textAlternative: "Add New Task" }} />
+
+<Story
+  name="Click"
+  args={{ textAlternative: "Add New Task" }}
+  play={async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const button = canvas.getByText("Add New Task");
+    await userEvent.click(button);
+  }}
+/>
---
+++ src/components/Interactive/IconButton/IconButton.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let textAlternative: string;
+</script>
+
+<button
+  on:click
+  class="bg-main-purple-normal hocus:bg-main-purple-hover outline-2 focus-visible:outline outline-offset-4 focus-visible:outline-main-purple-hover min-h-[2rem] inline-grid text-center place-items-center rounded-full px-4.5 motion-safe:transition-colors motion-safe:ease-in-out"
+>
+  <span aria-hidden="true"><slot /></span>
+  <span class="sr-only">{textAlternative}</span>
+</button>
