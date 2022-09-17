2fe8f304bde74d6016e04a99e02c47837118334e
Alexander <Sakeran@gmail.com>
2022-09-16 20:06:06 -0400

    Implement TodoCard component

---
+++ src/components/UI/TodoCard/TodoCard.stories.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+  import TodoCard from "./TodoCard.svelte";
+</script>
+
+<Meta
+  title="UI/TodoCard"
+  component={TodoCard}
+  argTypes={{
+    title: { control: "text" },
+    subtaskTotal: { control: "number" },
+    subtasksCompleted: { control: "number" },
+    click: { action: "click" },
+
+    containerWidthPx: { control: "number", defaultValue: 280 },
+  }}
+/>
+
+<Template let:args>
+  <div style={`max-width: ${args.containerWidthPx}px;`}>
+    <TodoCard {...args} on:click={args.click} />
+  </div>
+</Template>
+
+<Story
+  name="Default"
+  args={{
+    title: "Build UI for onboarding flow",
+    subtaskTotal: 3,
+    subtasksCompleted: 0,
+  }}
+/>
---
+++ src/components/UI/TodoCard/TodoCard.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Text from "../../Typography/Text/Text.svelte";
+  import Heading from "../../Typography/Heading/Heading.svelte";
+  import Card from "../Card/Card.svelte";
+
+  export let title: string;
+  export let subtaskTotal: number;
+  export let subtasksCompleted: number;
+</script>
+
+<Card
+  borderRadius={8}
+  padding={[23, 16]}
+  shadow={true}
+  classes="relative text-black dark:text-white hover:text-main-purple-normal focus-within:text-main-purple-normal dark:hover:text-main-purple-normal dark:focus-within:text-main-purple-normal"
+>
+  <Heading element={"h3"} style="M">{title}</Heading>
+  <Text style="M" classes="mt-2 text-gray-medium"
+    >{subtasksCompleted} of {subtaskTotal} complete</Text
+  >
+  <button
+    on:click
+    class="block w-full absolute inset-0 rounded-lg outline-2 focus-visible:outline outline-offset-4 focus-visible:outline-main-purple-hover motion-safe:transition-colors motion-safe:ease-in-out"
+    ><span class="sr-only">View Task</span></button
+  >
+</Card>
