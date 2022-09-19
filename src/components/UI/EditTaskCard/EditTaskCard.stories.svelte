29a74a18dd5c64985266618d1aff7b4faa573725
Alexander <Sakeran@gmail.com>
2022-09-19 15:17:50 -0400

    Implement EditTaskCard component

---
+++ src/components/UI/EditTaskCard/EditTaskCard.stories.svelte
@@ -0,0 +1,128 @@
+<script lang="ts">
+  import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+  import { userEvent, within } from "@storybook/testing-library";
+  import { expect } from "@storybook/jest";
+  import EditTaskCard from "./EditTaskCard.svelte";
+</script>
+
+<Meta
+  title="UI/EditTaskCard"
+  component={EditTaskCard}
+  argTypes={{
+    submitTask: { action: "submitTask" },
+  }}
+/>
+
+<Template let:args>
+  <div
+    style="max-width: 30rem; padding: 1rem; background-color: hsl(0 0% 0% / .4);"
+  >
+    <EditTaskCard {...args} on:submitTask={args.submitTask} />
+  </div>
+</Template>
+
+<Story
+  name="Default"
+  args={{
+    cardTitle: "Add New Task",
+    submitButtonText: "Create Task",
+
+    boardColumns: ["Todo", "Doing", "Done"],
+    status: "Todo",
+  }}
+/>
+
+<Story
+  name="Edit Existing Card"
+  args={{
+    cardTitle: "Edit Task",
+    submitButtonText: "Save Changes",
+
+    title: "Existing Task",
+    description: "Some existing task description",
+    subtasks: ["Task 1", "Task 2", "Task 3"],
+
+    boardColumns: ["Todo", "Doing", "Done"],
+    status: "Todo",
+  }}
+/>
+
+<Story
+  name="Create Task"
+  args={{
+    cardTitle: "Add New Task",
+    submitButtonText: "Create Task",
+
+    boardColumns: ["Todo", "Doing", "Done"],
+    status: "Todo",
+  }}
+  play={async ({ args, canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const createTaskFn = args.submitTask;
+
+    // Get Inputs
+    const titleInput = canvas.getByLabelText("Title");
+    const descInput = canvas.getByLabelText("Description");
+    const firstSubtaskInput = canvas.getByLabelText("blank subtask");
+    const addNewSubtaskButton = canvas.getByText("Add New Subtask");
+    const statusSelect = canvas.getByLabelText("Status");
+    const createTaskButton = canvas.getByText("Create Task");
+
+    // Should not submit without a title
+    await userEvent.click(createTaskButton);
+    expect(createTaskFn).not.toHaveBeenCalled();
+
+    await userEvent.type(titleInput, "Test Task");
+
+    // Should not submit without a subtask
+    await userEvent.click(createTaskButton);
+    expect(createTaskFn).not.toHaveBeenCalled();
+
+    // Input description (should be optional)
+    await userEvent.type(descInput, "Some task description");
+
+    // Input first subtask name
+    await userEvent.type(firstSubtaskInput, "Subtask 1");
+
+    // Clear name input -- should not submit
+    await userEvent.clear(titleInput);
+    await userEvent.click(createTaskButton);
+    expect(createTaskFn).not.toHaveBeenCalled();
+
+    // Re-type name
+    await userEvent.type(titleInput, "Test Task");
+
+    // Add new subtasks, plus one mistake
+    await userEvent.click(addNewSubtaskButton);
+    await userEvent.click(addNewSubtaskButton);
+    await userEvent.click(addNewSubtaskButton);
+
+    const newSubtaskInputs = canvas.getAllByLabelText("blank subtask");
+    expect(newSubtaskInputs).toHaveLength(3);
+
+    await userEvent.type(newSubtaskInputs[0], "Subtask 2");
+    await userEvent.type(newSubtaskInputs[1], "Mistake");
+    await userEvent.type(newSubtaskInputs[2], "Subtask 3");
+
+    // Delete mistake
+    const deleteMistakeButton = canvas.getByText("Delete subtask Mistake");
+    await userEvent.click(deleteMistakeButton);
+
+    // Update status to "Done"
+    await userEvent.selectOptions(statusSelect, "Done");
+
+    // Final submission should be valid and send the right data
+    await userEvent.click(createTaskButton);
+
+    expect(createTaskFn).toHaveBeenCalledTimes(1);
+    const { detail: newTaskData } = createTaskFn.mock.lastCall[0];
+
+    expect(newTaskData).toStrictEqual({
+      title: "Test Task",
+      description: "Some task description",
+      subtasks: ["Subtask 1", "Subtask 2", "Subtask 3"],
+      status: "Done",
+    });
+  }}
+/>
---
+++ src/components/UI/EditTaskCard/EditTaskCard.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
+  import Card from "../Card/Card.svelte";
+  import Button from "../../Interactive/Button/Button.svelte";
+  import InputList from "../../Interactive/InputList/InputList.svelte";
+  import TextInput from "../../Interactive/TextInput/TextInput.svelte";
+  import TextArea from "../../Interactive/TextArea/TextArea.svelte";
+  import Heading from "../../Typography/Heading/Heading.svelte";
+  import Select from "../../Interactive/Select/Select.svelte";
+
+  export let cardTitle: string = "Add New Task";
+  export let submitButtonText: string = "Create Task";
+  export let boardColumns: [string, ...string[]];
+
+  export let title = "";
+  export let description = "";
+  export let subtasks: string[] = [];
+  export let status: string = undefined;
+
+  if (!status) status = boardColumns[0];
+
+  const dispatch = createEventDispatcher();
+
+  function handleSubmitTask() {
+    if (!formIsValid()) return;
+    dispatch("submitTask", { title, description, subtasks, status });
+  }
+
+  function formIsValid() {
+    // Task title must not be blank
+    if (title.length === 0) return false;
+
+    // Require at least one subtask
+    if (subtasks.length < 1) return false;
+
+    const seenSubtasks = new Set();
+    for (const subtask of subtasks) {
+      // No empty subtask names
+      if (subtask.length === 0) return false;
+
+      // No duplicate subtask names allowed
+      if (seenSubtasks.has(subtask)) return false;
+      seenSubtasks.add(subtask);
+    }
+
+    // Status must be one of the provided values
+    if (!boardColumns.includes(status)) return false;
+
+    return true;
+  }
+</script>
+
+<Card borderRadius={6} paddingStyle="Modal">
+  <Heading classes="text-black dark:text-white">{cardTitle}</Heading>
+  <TextInput bind:value={title} label="Title" required={true} classes="mt-6" />
+  <TextArea label="Description" bind:value={description} classes="mt-6" />
+  <InputList
+    label="Subtasks"
+    buttonText="Add New Subtask"
+    itemName="subtask"
+    bind:items={subtasks}
+    classes="mt-6"
+  />
+  <Select options={boardColumns} label="Status" bind:value={status} classes="mt-6" />
+  <Button style="PrimaryS" classes="w-full mt-6" on:click={handleSubmitTask}
+    >{submitButtonText}</Button
+  >
+</Card>
