11971112795789db139696aa945576aede477e96
Alexander <Sakeran@gmail.com>
2022-09-22 17:56:06 -0400

    Implement Logo component

---
+++ src/components/Misc/Logo/Logo.stories.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+  import Logo from "./Logo.svelte";
+</script>
+
+<Meta title="Misc/Logo" component={Logo} />
+
+<Template>
+  <div><Logo /></div>
+</Template>
+
+<Story name="Default" />
