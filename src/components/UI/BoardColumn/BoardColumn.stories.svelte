561220aeac0e5a9dc8dafffa763aa5a4aac00480
Alexander <Sakeran@gmail.com>
2022-09-23 16:00:16 -0400

    Implement BoardColumn component

---
+++ src/components/UI/BoardColumn/BoardColumn.stories.svelte
@@ -0,0 +1,106 @@
+<script lang="ts">
+  import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+  import BoardColumn from "./BoardColumn.svelte";
+</script>
+
+<Meta
+  title="UI/BoardColumn"
+  component={BoardColumn}
+  argTypes={{
+    taskSelected: { action: "taskSelected" },
+  }}
+/>
+
+<Template let:args>
+  <div style={`max-width: 17.5rem;`}>
+    <BoardColumn {...args} on:taskSelected={args.taskSelected} />
+  </div>
+</Template>
+
+<Story
+  name="Empty"
+  args={{
+    columnData: {
+      name: "Todo",
+      boardColor: "#49C4E5",
+      tasks: [],
+    },
+  }}
+/>
+
+<Story
+  name="With Tasks"
+  args={{
+    columnData: {
+      name: "Todo",
+      boardColor: "#49C4E5",
+      tasks: [
+        {
+          id: "1",
+          title: "Build UI for onboarding flow",
+          description: "",
+          status: "Todo",
+          subtasks: [
+            {
+              title: "Sign up page",
+              isCompleted: true,
+            },
+            {
+              title: "Sign in page",
+              isCompleted: false,
+            },
+            {
+              title: "Welcome page",
+              isCompleted: false,
+            },
+          ],
+        },
+        {
+          id: "2",
+          title: "Build UI for search",
+          description: "",
+          status: "Todo",
+          subtasks: [
+            {
+              title: "Search page",
+              isCompleted: false,
+            },
+          ],
+        },
+        {
+          id: "3",
+          title: "Build settings UI",
+          description: "",
+          status: "Todo",
+          subtasks: [
+            {
+              title: "Account page",
+              isCompleted: false,
+            },
+            {
+              title: "Billing page",
+              isCompleted: false,
+            },
+          ],
+        },
+        {
+          id: "4",
+          title: "QA and test all major user journeys",
+          description:
+            "Once we feel version one is ready, we need to rigorously test it both internally and externally to identify any major gaps.",
+          status: "Todo",
+          subtasks: [
+            {
+              title: "Internal testing",
+              isCompleted: false,
+            },
+            {
+              title: "External testing",
+              isCompleted: false,
+            },
+          ],
+        },
+      ],
+    },
+  }}
+/>
---
+++ src/components/UI/BoardColumn/BoardColumn.svelte
@@ -0,0 +1,60 @@
+<script context="module" lang="ts">
+  // TODO - Factor all types into a single location
+  type Subtask = { title: string; isCompleted: string };
+
+  type Task = {
+    id: string;
+    title: string;
+    description: string;
+    status: string;
+    subtasks: Subtask[];
+  };
+
+  type BoardColumn = {
+    name: string;
+    boardColor: string;
+    tasks: Task[];
+  };
+</script>
+
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
+  import Heading from "../../Typography/Heading/Heading.svelte";
+  import TodoCard from "../TodoCard/TodoCard.svelte";
+
+  export let columnData: BoardColumn;
+
+  const dispatch = createEventDispatcher();
+  function handleTaskSelect(taskId: string) {
+    dispatch("taskSelected", taskId);
+  }
+</script>
+
+<div>
+  <div class="flex gap-3 items-center">
+    <div
+      style:background-color={columnData.boardColor || "#49C4E5"}
+      class="w-[0.9375rem] aspect-square rounded-round"
+      aria-hidden="true"
+    />
+    <Heading style="S" element="h3" classes="uppercase text-gray-medium">
+      {columnData.name}
+      {columnData.tasks.length ? `(${columnData.tasks.length})` : ""}
+    </Heading>
+  </div>
+
+  <div class="mt-6 flex flex-col gap-5">
+    {#each columnData.tasks as task}
+      <div>
+        <TodoCard
+          on:click={() => handleTaskSelect(task.id)}
+          title={task.title}
+          subtaskTotal={task.subtasks.length}
+          subtasksCompleted={task.subtasks.filter((st) => st.isCompleted)
+            .length}
+        />
+      </div>
+    {/each}
+  </div>
+</div>
