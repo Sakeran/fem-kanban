bc945029c25946cb16a3412b54447e26c6c7f87c
Alexander <Sakeran@gmail.com>
2022-09-24 13:20:52 -0400

    Update BoardColumn story to use shared mock data

--- src/components/UI/BoardColumn/BoardColumn.stories.svelte
+++ src/components/UI/BoardColumn/BoardColumn.stories.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
   import BoardColumn from "./BoardColumn.svelte";
+
+  import boardData from "../../../helpers/mockData.json";
 </script>
 
 <Meta
@@ -31,76 +33,6 @@
 <Story
   name="With Tasks"
   args={{
-    columnData: {
-      name: "Todo",
-      boardColor: "#49C4E5",
-      tasks: [
-        {
-          id: "1",
-          title: "Build UI for onboarding flow",
-          description: "",
-          status: "Todo",
-          subtasks: [
-            {
-              title: "Sign up page",
-              isCompleted: true,
-            },
-            {
-              title: "Sign in page",
-              isCompleted: false,
-            },
-            {
-              title: "Welcome page",
-              isCompleted: false,
-            },
-          ],
-        },
-        {
-          id: "2",
-          title: "Build UI for search",
-          description: "",
-          status: "Todo",
-          subtasks: [
-            {
-              title: "Search page",
-              isCompleted: false,
-            },
-          ],
-        },
-        {
-          id: "3",
-          title: "Build settings UI",
-          description: "",
-          status: "Todo",
-          subtasks: [
-            {
-              title: "Account page",
-              isCompleted: false,
-            },
-            {
-              title: "Billing page",
-              isCompleted: false,
-            },
-          ],
-        },
-        {
-          id: "4",
-          title: "QA and test all major user journeys",
-          description:
-            "Once we feel version one is ready, we need to rigorously test it both internally and externally to identify any major gaps.",
-          status: "Todo",
-          subtasks: [
-            {
-              title: "Internal testing",
-              isCompleted: false,
-            },
-            {
-              title: "External testing",
-              isCompleted: false,
-            },
-          ],
-        },
-      ],
-    },
+    columnData: boardData.boards[0].columns[0],
   }}
 />
