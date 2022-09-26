b92ba7e40e12fcff4235f7888f6fbf382660b063
Alexander <Sakeran@gmail.com>
2022-09-26 10:04:43 -0400

    Remove scrolling container from BoardColumns

--- src/components/UI/BoardColumns/BoardColumns.svelte
+++ src/components/UI/BoardColumns/BoardColumns.svelte
@@ -4,56 +4,31 @@
   import BoardColumn from "../BoardColumn/BoardColumn.svelte";
 
   export let board: Board;
-  export let sidebarExpanded: boolean = true;
+  export let sidebarExpanded: boolean = false;
 </script>
 
-<!-- Scrolling Container -->
-<div class="scrolling-container px-4 sm:px-6 py-6 overflow-auto">
-  <!-- Columns Container -->
-  <div
-    data-sidebar-expanded={sidebarExpanded || null}
-    class="columns-container flex gap-6 motion-safe:transition-transform motion-safe:ease-in-out"
-  >
-    {#each board.columns as column}
-      <div class="basis-[17.5rem] shrink-0">
-        <BoardColumn columnData={column} />
-      </div>
-    {/each}
-    <!-- Add Columns Button -->
-    <div class="relative basis-[17.5rem] shrink-0">
-      <button
-        class="new-column-button absolute top-0 bottom-0 left-0 w-full rounded-sm grid place-items-center text-gray-medium hocus:text-main-purple-normal"
-        ><Heading style="XL" classes="z-10"
-          ><span aria-hidden="true">+</span> New Column</Heading
-        ></button
-      >
+<!-- Columns Container -->
+<div
+  data-sidebar-expanded={sidebarExpanded || null}
+  class="columns-container flex gap-6 motion-safe:transition-transform motion-safe:ease-in-out"
+>
+  {#each board.columns as column}
+    <div class="basis-[17.5rem] shrink-0">
+      <BoardColumn columnData={column} />
     </div>
+  {/each}
+  <!-- Add Columns Button -->
+  <div class="relative basis-[17.5rem] shrink-0">
+    <button
+      class="new-column-button absolute top-0 bottom-0 left-0 w-full rounded-sm grid place-items-center text-gray-medium hocus:text-main-purple-normal"
+      ><Heading style="XL" classes="z-10"
+        ><span aria-hidden="true">+</span> New Column</Heading
+      ></button
+    >
   </div>
 </div>
 
 <style lang="postcss">
-  .scrolling-container::-webkit-scrollbar {
-    width: 0.5rem;
-    height: 0.5rem;
-    background-color: theme("colors.main-purple.normal" / 0.5);
-
-    border-radius: 4px;
-  }
-
-  .scrolling-container::-webkit-scrollbar-thumb {
-    background-color: theme("colors.main-purple.normal");
-
-    border-radius: 4px;
-  }
-
-  @supports (scrollbar-width: thin) {
-    .scrolling-container {
-      scrollbar-color: theme("colors.main-purple.normal")
-        theme("colors.main-purple.normal" / 0.5);
-      scrollbar-width: thin;
-    }
-  }
-
   .columns-container {
     transform: translateX(0);
   }
