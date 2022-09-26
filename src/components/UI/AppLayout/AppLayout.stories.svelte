6fb29da2f140792a360f4049d7f0dd624b95ecc6
Alexander <Sakeran@gmail.com>
2022-09-26 10:09:31 -0400

    Implement presentational AppLayout component

---
+++ src/components/UI/AppLayout/AppLayout.stories.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+  import AppLayout from "./AppLayout.svelte";
+
+  import boardData from "../../../helpers/mockData.json";
+</script>
+
+<Meta title="UI/AppLayout" component={AppLayout} />
+
+<Template let:args>
+  <AppLayout {...args} />
+</Template>
+
+<Story
+  name="Default"
+  args={{ boardData: boardData, currentBoard: boardData.boards[0] }}
+  parameters={{ layout: "fullscreen" }}
+/>
---
+++ src/components/UI/AppLayout/AppLayout.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import type { Board } from "src/types/boardTypes";
+  import BoardColumns from "../BoardColumns/BoardColumns.svelte";
+  import Header from "../Header/Header.svelte";
+  import Sidebar from "../Sidebar/Sidebar.svelte";
+
+  // TODO - These top-level components should be more tightly coupled
+  // to the data layer.
+  export let boardData: { boards: Board[] };
+  export let currentBoard: Board;
+</script>
+
+<div class="app-layout">
+  <div class="app-layout-header"><Header /></div>
+  <div class="app-layout-content">
+    <Sidebar {boardData} />
+    <div class="absolute inset-0 px-4 sm:px-6 py-6 overflow-auto z-0">
+      <BoardColumns board={currentBoard} />
+    </div>
+  </div>
+</div>
+
+<style lang="postcss">
+  .app-layout {
+    display: flex;
+    flex-direction: column;
+
+    min-height: 100vh;
+  }
+
+  .app-layout-header {
+  }
+
+  .app-layout-content {
+    position: relative;
+    flex-grow: 1;
+  }
+</style>
