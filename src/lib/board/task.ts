c2c885ddaf4b0c9198c16ea49376fc87896646f9
Alexander <Sakeran@gmail.com>
2022-10-02 14:06:12 -0400

    Add update method to Task

--- src/lib/board/task.ts
+++ src/lib/board/task.ts
@@ -55,19 +55,77 @@ export default class Task {
     return this.parentColumn;
   }
 
-  updateStatus(newStatus: string) {
-    // Check that status is valid.
-    for (const { name } of this.board.columns) {
-      if (name === newStatus) {
-        console.warn(
-          `Tried to update task's status to ${newStatus}, but that column doesn't exist.`
+  update({ title, description, subtasks, status }) {
+    let updatedTask = false;
+
+    if (title) {
+      this.updateTitle(title);
+      updatedTask = true;
+    }
+
+    if (description) {
+      this.updateDescription(description);
+      updatedTask = true;
+    }
+
+    if (status) {
+      this.updateStatus(status);
+      updatedTask = true;
+    }
+
+    if (subtasks) {
+      const newSubtasks = subtasks.map((st) => {
+        const existing = this.subtasks.find(
+          (existing) => existing.title === st
         );
-        return;
+
+        if (existing) return existing;
+
+        return { title: st, isCompleted: false };
+      });
+
+      // Subtasks are updated if there are more or fewer of them, or if the subtasks
+      // have been reordered.
+      if (newSubtasks.length !== this.subtasks.length) {
+        this.data.subtasks = newSubtasks;
+        updatedTask = true;
+      } else {
+        for (let i = 0; i < newSubtasks.length; i++) {
+          if (newSubtasks[i].title !== this.subtasks[i].title) {
+            this.data.subtasks = newSubtasks;
+            updatedTask = true;
+            break;
+          }
+        }
       }
     }
 
+    if (updatedTask) {
+      eventBus.dispatch("taskUpdated", this);
+    }
+  }
+
+  updateTitle(newTitle: string) {
+    if (newTitle && newTitle !== this.title) {
+      this.data.title = newTitle;
+    }
+  }
+
+  updateDescription(newDescription: string) {
+    if (newDescription && newDescription !== this.description) {
+      this.data.description = newDescription;
+    }
+  }
+
+  updateStatus(newStatus: string) {
+    // Check that status is valid.
+    if (!this.checkValidStatus(newStatus)) {
+      console.warn(
+        `Tried to update task's status to ${newStatus}, but that column doesn't exist.`
+      );
+    }
+
     this.data.status = newStatus;
-    eventBus.dispatch("taskUpdated", this);
   }
 
   toggleSubtask(subtaskTitle: string) {
@@ -82,6 +140,11 @@ export default class Task {
 
     subtask.isCompleted = !subtask.isCompleted;
     eventBus.dispatch("taskUpdated", this);
+  }
+
+  checkValidStatus(status) {
+    // Check that status is valid.
+    return this.board.columnNames.includes(status);
   }
 
   serializeToData() {
